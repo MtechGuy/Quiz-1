e0fd929d8a1d510b0d7925a7c7ef70586dd9f7a4
Alex A. Peraza <2020152022@ub.edu.bz>
2023-01-28 12:27:30 -0600

    Question 10

--- circle_test.go
+++ circle_test.go
@@ -1,1 +1,21 @@
 package main
+
+import (
+	"testing"
+)
+
+func TestCircleArea(t *testing.T) {
+	cir := circle{radius: 5}
+	result := cir.area()
+	if result != 78.53981633974483 {
+		t.Errorf("Expected area of 78.53981633974483, got %v", result)
+	}
+}
+
+func TestCirclePerimeter(t *testing.T) {
+	cir := circle{radius: 5}
+	result := cir.perimeter()
+	if result != 31.41592653589793 {
+		t.Errorf("Expected perimeter of 31.41592653589793")
+	}
+}
