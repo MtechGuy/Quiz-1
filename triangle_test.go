e21ec99ce5811f2ddb474afe43c546b10f83e8e1
Alex A. Peraza <2020152022@ub.edu.bz>
2023-01-28 12:24:39 -0600

    Question 10

--- triangle_test.go
+++ triangle_test.go
@@ -1,3 +1,21 @@
 package main
 
+import (
+	"testing"
+)
 
+func TestTriangleArea(t *testing.T) {
+	tri := triangle{base: 3, height: 4}
+	result := tri.area()
+	if result != 6 {
+		t.Errorf("Expected area of 6, got %v", result)
+	}
+}
+
+func TestTrianglePerimeter(t *testing.T) {
+	tri := triangle{base: 3, height: 4}
+	result := tri.perimeter()
+	if result != 12 {
+		t.Errorf("Expected perimeter of 12, got %v", result)
+	}
+}
