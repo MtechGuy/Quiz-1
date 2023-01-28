425855b12aa295f5cc391c4bdd78beb1ccd8f7c6
Alex A. Peraza <2020152022@ub.edu.bz>
2023-01-28 12:07:24 -0600

    Question 3

--- triangle.go
+++ triangle.go
@@ -12,3 +12,8 @@ type triangle struct {
 func (t triangle) area() float64 {
 	return 0.5 * t.base * t.height
 }
+
+func (t triangle) perimeter() float64 {
+	a := math.Sqrt(math.Pow(t.base, 2) + math.Pow(t.height, 2))
+	return a + t.base + t.height
+}
