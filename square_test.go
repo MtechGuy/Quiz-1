1b9639ff850684559b02ff93a7f593f4d363c04a
Alex A. Peraza <2020152022@ub.edu.bz>
2023-01-28 12:29:28 -0600

    Question 11

--- square_test.go
+++ square_test.go
@@ -1,1 +1,15 @@
 package main
+
+import (
+	"testing"
+)
+
+func TestSquare(t *testing.T) {
+	a, p := square(5)
+	if a != 25 {
+		t.Errorf("Expected area of 25, got %v", a)
+	}
+	if p != 20 {
+		t.Errorf("Expected perimeter of 20, got %v", p)
+	}
+}
