708706c4b4cf4c8e332670cf191161228a3ca4a2
Alex Peraza <122631126+MtechGuy@users.noreply.github.com>
2023-01-29 14:59:50 -0600

    Update circle_test.go

--- circle_test.go
+++ circle_test.go
@@ -16,6 +16,6 @@ func TestCirclePerimeter(t *testing.T) {
 	cir := circle{radius: 5}
 	result := cir.perimeter()
 	if result != 31.41592653589793 {
-		t.Errorf("Expected perimeter of 31.41592653589793")
+		t.Errorf("Expected perimeter of 31.41592653589793, got %v", result)
 	}
 }
